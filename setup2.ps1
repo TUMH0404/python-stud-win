3dd0d63ccb2f337e25016c58d7f3b48954aa23d2
Tomoya Ohnishi <t.ohnishi@tumh.ac.jp>
2026-04-21 11:36:14 +0900

    Add files via upload

---
+++ setup2.ps1
@@ -0,0 +1,19 @@
+$TARGET = "$env:USERPROFILE\Documents\python-stud"
+$GIT    = "$env:ProgramFiles\Git\bin\git.exe"
+
+if (Test-Path $TARGET) {
+    Remove-Item -Recurse -Force $TARGET
+    Write-Host "Folder deleted."
+} else {
+    Write-Host "Folder does not exist."
+}
+
+Set-Location "$env:USERPROFILE\Documents"
+
+if (Test-Path $GIT) {
+    & $GIT clone https://github.com/TUMH0404/python-stud.git
+} else {
+    Write-Host "Git executable not found."
+}
+
+Set-Location "$env:USERPROFILE\Documents\python-stud"
